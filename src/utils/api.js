bfefb9f80aad3147d36471fed7f8fe190780ebb4
ZachHung <zachhung911@gmail.com>
2022-04-29 08:52:07 +0700

    fixed mixed content request

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = `http://nhom01.somee.com/Api/`;
+const BASE_URL = `https://nhom01.somee.com/Api/`;
 
 export const Request = axios.create({
   baseURL: BASE_URL,
